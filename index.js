f3ac4794fc028690b549b22fe67b2bbea14bc6fd
Brandon Farmer <bthesorceror@gmail.com>
2013-01-01 23:55:28 -0500

    Initial Commit

---
+++ index.js
@@ -0,0 +1,24 @@
+var fs = require('fs'),
+    mime = require('mime');
+
+function FabricSoftener(starting_path) {
+  this.starting_path = starting_path;
+}
+
+FabricSoftener.prototype.middleware = function() {
+  var self = this;
+  return function(req, res, next) {
+    var path = self.starting_path + req.url;
+    fs.exists(path, function(exists) {
+      if (exists) {
+        res.writeHead(200, { 'Content-Type': mime.lookup(path) });
+        var file = fs.createReadStream(path);
+        file.pipe(res);
+      } else {
+        next();
+      }
+    });
+  }
+}
+
+module.exports = FabricSoftener;
