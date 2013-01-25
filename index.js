e0fb4433ab7a60d38df0a0018d3fbc98e8c1183d
Brandon Farmer <bthesorceror@gmail.com>
2013-01-24 21:47:24 -0500

    Refactor how rendering happens

--- index.js
+++ index.js
@@ -1,25 +1,33 @@
 var fs = require('fs'),
     mime = require('mime');
 
-function FabricSoftener(starting_path) {
+function FabricSoftener(starting_path, options) {
+  options = options || {};
   this.starting_path = starting_path;
+  this.prefix = options['prefix'] || "/static";
+  this.matcher = new RegExp("^" + this.prefix);
+}
+
+function renderFile(res, path) {
+  res.writeHead(200, { 'Content-Type': mime.lookup(path) });
+  var file = fs.createReadStream(path);
+  file.pipe(res);
 }
 
 FabricSoftener.prototype.middleware = function() {
   var self = this;
   return function(req, res, next) {
-    var path = self.starting_path + req.url;
-    fs.exists(path, function(exists) {
+    if (req.url.match(self.matcher)) {
+      var sub_path = req.url.replace(self.prefix, ''),
+          path = self.starting_path + sub_path;
       fs.stat(path, function(err, stats) {
-        if (exists && !stats.isDirectory()) {
-          res.writeHead(200, { 'Content-Type': mime.lookup(path) });
-          var file = fs.createReadStream(path);
-          file.pipe(res);
-        } else {
-          next();
+        if (!stats.isDirectory()) {
+          renderFile(res, path);
         }
       });
-    });
+    } else {
+      next();
+    }
   }
 }
 
